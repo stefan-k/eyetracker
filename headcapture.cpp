9e2ecd97cad4cdaf8d3b8dfa717258aeedb16632
Stefan <me@stefan-kroboth.com>
2011-12-08 19:31:45 +0100

    forgot files...

---
+++ headcapture.cpp
@@ -0,0 +1,39 @@
+#include "headcapture.h"
+
+#include <iostream>
+
+// opencv
+#include "highgui.h"
+
+//------------------------------------------------------------------------------
+HeadCapture::HeadCapture(const unsigned cameraID, const bool convert_to_gray)
+  : m_convert_to_gray(convert_to_gray)
+{
+  m_eye = new cv::VideoCapture(cameraID);
+
+  if (!(m_eye->isOpened()))
+  {
+    std::cout << "[Error] cannot open video file";
+  }
+
+  // Get video information
+  m_eye_width = (int)m_eye->get(CV_CAP_PROP_FRAME_WIDTH);
+  m_eye_height = (int)m_eye->get(CV_CAP_PROP_FRAME_HEIGHT);
+
+}
+
+//------------------------------------------------------------------------------
+HeadCapture::~HeadCapture()
+{
+  delete m_eye;
+}
+
+//------------------------------------------------------------------------------
+cv::Mat HeadCapture::getFrame()
+{
+  *(m_eye) >> m_frame;
+  if(m_convert_to_gray)
+    cv::cvtColor(m_frame.clone(), m_frame, CV_BGR2GRAY);
+
+  return m_frame;
+}
---
+++ headcapture.h
@@ -0,0 +1,67 @@
+/**
+ * @file headcapture.h
+ * @brief Captures the frames of the eye
+ *
+ * Image Processing and Pattern Recognition
+ * (Bildverarbeitung und Mustererkennung)
+ * Project
+ */
+
+#ifndef __HEADCAPTURE_H__
+#define __HEADCAPTURE_H__
+
+// stl
+#include <vector>
+
+// opencv
+#include <cv.h>
+
+class HeadCapture
+{
+  cv::VideoCapture* m_eye;
+  //cv::VideoCapture eye;
+  cv::Mat m_frame;
+  unsigned m_eye_height;
+  unsigned m_eye_width;
+  const bool m_convert_to_gray;
+
+public:
+
+  /**
+   * Constructor
+   * @param 
+   */
+  HeadCapture(const unsigned cameraID, const bool convert_to_gray);
+
+  /**
+   * Destructor
+   */
+  virtual ~HeadCapture();
+
+  /**
+   * getFrame
+   * @param 
+   */
+  cv::Mat getFrame();
+  
+  
+private:
+
+  /**
+   * disabled default constructor
+   */
+  HeadCapture();
+
+  /**
+   * disabled copy constructor
+   */
+  HeadCapture(const HeadCapture&);
+  
+  /**
+   * disabled copy constructor
+   */
+  HeadCapture& operator = (const HeadCapture&);
+  
+};
+
+#endif //__HEADCAPTURE_H__
